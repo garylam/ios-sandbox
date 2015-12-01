954a601800fb2581dc45616981360788fd7aa203
glam <glam@glam-mbp101.edmunds.hq>
2015-11-30 22:08:42 -0800

    Revert "November commit."
    This reverts commit fe449403809bbc79167409a3394c89754f895309.

--- teamtreehouse/Operator-Precedence.playground/Contents.swift
+++
@@ -1,5 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var x = 100 + 100 - 5 * 2 / 3 % 7
--- teamtreehouse/Strings.playground/Contents.swift
+++
@@ -1,5 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-print("List:\n1\t🍎\n2\t🍌\n6\t🍊")
